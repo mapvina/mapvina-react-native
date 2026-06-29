3b3adae77fbf98c86d30ac74ebfe066a10195921
Sunny0025-IT <Sunny0025-IT@users.noreply.github.com>
2026-06-29 21:42:46 +0700

    fix: add React/RCTFollyConvert.h fallback for RN <0.80 compatibility
    When using the library with React Native <0.80 (e.g. Expo SDK 53 with RN 0.79),
    react/utils/FollyConvert.h does not exist. Add fallback to React/RCTFollyConvert.h
    which is the correct path in older React Native versions.

--- package/ios/utils/MLRNFollyConvert.h
+++ package/ios/utils/MLRNFollyConvert.h
@@ -1,5 +1,7 @@
 #if __has_include(<react/utils/FollyConvert.h>)
 #import <react/utils/FollyConvert.h>
+#elif __has_include(<React/RCTFollyConvert.h>)
+#import <React/RCTFollyConvert.h>
 #elif __has_include("FollyConvert.h")
 #import "FollyConvert.h"
 #endif
